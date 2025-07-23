7e7dd06d305e16872eb0e527ec141e404db6cb65
AidenLee76 <masklee76@gmail.com>
2025-07-23 21:57:37 +0900

    HikRobotCam 구현

--- JidamVision4/Core/InspStage.cs
+++ JidamVision4/Core/InspStage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JidamVision4.Grab;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Drawing;
@@ -11,6 +12,10 @@ namespace JidamVision4.Core
     //검사와 관련된 클래스를 관리하는 클래스
     public class InspStage : IDisposable
     {
+        public static readonly int MAX_GRAB_BUF = 5;
+
+        private ImageSpace _imageSpace = null;
+        private HikRobotCam _grabManager = null;
         SaigeAI _saigeAI; // SaigeAI 인스턴스
 
         public InspStage() { }
@@ -26,8 +31,89 @@ namespace JidamVision4.Core
 
         public bool Initialize()
         {
+            _grabManager = new HikRobotCam();
+
+            if (_grabManager.InitGrab() == true)
+            {
+                _grabManager.TransferCompleted += _multiGrab_TransferCompleted;
+
+                InitModelGrab(MAX_GRAB_BUF);
+            }
 
             return true;
+        }
+
+        public void InitModelGrab(int bufferCount)
+        {
+            if (_grabManager == null)
+                return;
+
+            int pixelBpp = 8;
+            _grabManager.GetPixelBpp(out pixelBpp);
+
+            int inspectionWidth;
+            int inspectionHeight;
+            int inspectionStride;
+            _grabManager.GetResolution(out inspectionWidth, out inspectionHeight, out inspectionStride);
+
+            if (_imageSpace != null)
+            {
+                _imageSpace.SetImageInfo(pixelBpp, inspectionWidth, inspectionHeight, inspectionStride);
+            }
+
+            SetBuffer(bufferCount);
+
+            //_grabManager.SetExposureTime(25000);
+
+        }
+
+        public void SetBuffer(int bufferCount)
+        {
+            if (_grabManager == null)
+                return;
+
+            if (_imageSpace.BufferCount == bufferCount)
+                return;
+
+            _imageSpace.InitImageSpace(bufferCount);
+            _grabManager.InitBuffer(bufferCount);
+
+            for (int i = 0; i < bufferCount; i++)
+            {
+                _grabManager.SetBuffer(
+                    _imageSpace.GetInspectionBuffer(i),
+                    _imageSpace.GetnspectionBufferPtr(i),
+                    _imageSpace.GetInspectionBufferHandle(i),
+                    i);
+            }
+        }
+
+
+        public void Grab(int bufferIndex)
+        {
+            if (_grabManager == null)
+                return;
+
+            _grabManager.Grab(bufferIndex, true);
+        }
+
+        private void _multiGrab_TransferCompleted(object sender, object e)
+        {
+            int bufferIndex = (int)e;
+            Console.WriteLine($"_multiGrab_TransferCompleted {bufferIndex}");
+
+            _imageSpace.Split(bufferIndex);
+
+            DisplayGrabImage(bufferIndex);
+        }
+
+        private void DisplayGrabImage(int bufferIndex)
+        {
+            var cameraForm = MainForm.GetDockForm<CameraForm>();
+            if (cameraForm != null)
+            {
+                cameraForm.UpdateDisplay();
+            }
         }
 
         public void UpdateDisplay(Bitmap bitmap)
