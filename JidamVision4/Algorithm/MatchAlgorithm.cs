0c2c1ffb0a2938e56bdf1df0f3d890579e76c29a
AidenLee76 <masklee76@gmail.com>
2026-01-12 11:23:57 +0900

    오타 수정

--- JidamVision4/Algorithm/MatchAlgorithm.cs
+++ JidamVision4/Algorithm/MatchAlgorithm.cs
@@ -14,7 +14,7 @@ namespace JidamVision4.Algorithm
     템플릿 매칭 클래스를 구현하고, 매칭에 필요한 이미지 연동
     1) MatchAlgorithm 클래스 생성 및 구현, InspAlgorithm 상속 받기
     2) UIControl / PatternImageEditor 클래스 구현 - 템플릿 이미지 편집을 위한 컨트롤
-    3) Propety / MatchInspProp UserControl 생성 - 템플릿 매칭 속성 편집을 위한 컨트롤
+    3) Property / MatchInspProp UserControl 생성 - 템플릿 매칭 속성 편집을 위한 컨트롤
     4) #11_MATCHING#1 ~ 12 구현
     */
 
